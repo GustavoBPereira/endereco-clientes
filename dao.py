2a092a09189cf3149f6dde9a92c3382761108da3
Gustavo Brito <elodin2134@gmail.com>
2019-02-15 08:56:50 -0200

    feat(database access) migrando de mysql para sqlite3 por ser uma aplicação simples

--- dao.py
+++ dao.py
@@ -1,34 +1,57 @@
+import sqlite3
+
 class Dados:
     def __init__(self, db):
         self.db = db
-        self.cursor = self.db.cursor()
 
     def buscar(self, numero):
-        self.cursor.execute(f'SELECT * FROM dados where numero_contato={numero}')
-        return self.cursor.fetchone()
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f'SELECT * FROM enderecos where numero_contato=?',(numero,))
+        resultado = cursor.fetchone()
+        con.close()
+        return resultado
 
     def registrar(self, numero, nome, rua, lote, quadra, referencia):
-        self.cursor.execute(f"INSERT INTO dados(numero_contato, nome, rua, lote, quadra, referencia) values('{numero}', '{nome}', '{rua}', '{lote}', '{quadra}', '{referencia}')")
-        self.db.commit()
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f"INSERT INTO enderecos(numero_contato, nome, rua, lote, quadra, referencia) values('{numero}', '{nome}', '{rua}', '{lote}', '{quadra}', '{referencia}')")
+        con.commit()
+        con.close()
         return True
     
     def alterar(self,numero_contato,nome,rua,lote,quadra,referencia):
-        self.cursor.execute(f"UPDATE dados SET nome='{nome}', rua='{rua}', lote='{lote}', quadra='{quadra}', referencia='{referencia}' WHERE numero_contato='{numero_contato}'")
-        self.db.commit()
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f"UPDATE enderecos SET nome='{nome}', rua='{rua}', lote='{lote}', quadra='{quadra}', referencia='{referencia}' WHERE numero_contato='{numero_contato}'")
+        con.commit()
+        con.close()
         return True
 
     def todos_clientes(self):
-        self.cursor.execute(f"SELECT * FROM dados ORDER BY nome")
-        dados = self.cursor.fetchall()
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM enderecos ORDER BY nome")
+        dados = cursor.fetchall()
+        con.close()
         return dados
+
+
+        dados = self.cursor.fetchall()
     
     def remover(self, numero):
-        self.cursor.execute(f"delete from dados where numero_contato='{numero}'")
-        self.db.commit()
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f"delete from enderecos where numero_contato=?",(numero,))
+        con.commit()
+        con.close()
         return True
     
     def contar(self):
-        self.cursor.execute(f"SELECT COUNT(*) FROM dados")
-        tot = str(self.cursor.fetchone())
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
+        cursor.execute(f"SELECT COUNT(*) FROM enderecos")
+        tot = str(cursor.fetchone())
         tot = tot.replace('(','').replace(',','').replace(')','')
+        con.close()
         return tot 
